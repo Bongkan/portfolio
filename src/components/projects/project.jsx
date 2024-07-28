cc4dfd7aa897af5d64b8e9b6c0f28b3c6ebc0d25
Bongkan <bongkan.jj@gmail.com>
2024-07-28 23:32:33 +0700

    responsive

--- src/components/projects/project.jsx
+++ src/components/projects/project.jsx
@@ -6,25 +6,21 @@ import crud from "../../assets/projects/SimpleCRUD.png";
 const Project = ({ showProject }) => {
   return (
     <div className="pt-10 flex flex-col md:flex-row justify-around w-full pr-10">
-      {showProject === "Pony Mart" && (
+      <div className="block md:hidden">
         <div>
           <a href="https://jsd-7-cmfm-frontend.vercel.app/" target="_blank">
-            <img src={ponyMart} alt="ponyMart" className="h-2/3" />
+            <img src={ponyMart} alt="Pony Mart" className="h-2/3" />
           </a>
           <p className="text-center pt-3">
             E-commerce website, a group project created using the MERN stack
           </p>
         </div>
-      )}
-      {showProject === "CRUD" && (
         <div>
           <a href="https://05-fear-js.vercel.app/" target="_blank">
             <img src={crud} alt="CRUD" className="h-2/3" />
           </a>
           <p className="text-center pt-3">Simple CRUD manipulation</p>
         </div>
-      )}
-      {showProject === "Colmar" && (
         <div>
           <a href="https://5-fear-colmar.vercel.app/" target="_blank">
             <img src={colmar} alt="Colmar" className="h-2/3" />
@@ -33,7 +29,37 @@ const Project = ({ showProject }) => {
             My very first front-end project in this journey
           </p>
         </div>
-      )}
+      </div>
+      <div className="hidden md:block">
+        {showProject === "Pony Mart" && (
+          <div>
+            <a href="https://jsd-7-cmfm-frontend.vercel.app/" target="_blank">
+              <img src={ponyMart} alt="Pony Mart" className="h-2/3" />
+            </a>
+            <p className="text-center pt-3">
+              E-commerce website, a group project created using the MERN stack
+            </p>
+          </div>
+        )}
+        {showProject === "CRUD" && (
+          <div>
+            <a href="https://05-fear-js.vercel.app/" target="_blank">
+              <img src={crud} alt="CRUD" className="h-2/3" />
+            </a>
+            <p className="text-center pt-3">Simple CRUD manipulation</p>
+          </div>
+        )}
+        {showProject === "Colmar" && (
+          <div>
+            <a href="https://5-fear-colmar.vercel.app/" target="_blank">
+              <img src={colmar} alt="Colmar" className="h-2/3" />
+            </a>
+            <p className="text-center pt-3">
+              My very first front-end project in this journey
+            </p>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
