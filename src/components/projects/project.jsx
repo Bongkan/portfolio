74ee896e0d399cb95d57234c7eb63304b5280a97
Bongkan <bongkan.jj@gmail.com>
2024-07-29 04:24:55 +0700

    change about page's font and project description

--- src/components/projects/project.jsx
+++ src/components/projects/project.jsx
@@ -5,29 +5,55 @@ import crud from "../../assets/projects/SimpleCRUD.png";
 
 const Project = ({ showProject }) => {
   return (
-    <div className="pt-10 flex flex-col md:flex-row justify-around w-full pr-10 animation-fade-in">
-      <div className="block md:hidden">
-        <div>
-          <a href="https://jsd-7-cmfm-frontend.vercel.app/" target="_blank">
-            <img src={ponyMart} alt="Pony Mart" className="h-2/3" />
-          </a>
-          <p className="text-center pt-3">
-            E-commerce website, a group project created using the MERN stack
-          </p>
-        </div>
-        <div>
-          <a href="https://05-fear-js.vercel.app/" target="_blank">
-            <img src={crud} alt="CRUD" className="h-2/3" />
-          </a>
-          <p className="text-center pt-3">Simple CRUD manipulation</p>
-        </div>
-        <div>
-          <a href="https://5-fear-colmar.vercel.app/" target="_blank">
-            <img src={colmar} alt="Colmar" className="h-2/3" />
-          </a>
-          <p className="text-center pt-3">
-            My very first front-end project in this journey
-          </p>
+    <div className="pt-10 flex flex-col md:flex-row justify-around w-full pr-5 md:pr-10 animation-fade-in">
+      <div className="flex items-center justify-center min-h-screen">
+        <div className="block md:hidden h-full max-w-sm py-10">
+          <div className="h-1/3">
+            <a
+              href="https://jsd-7-cmfm-frontend.vercel.app/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src={ponyMart}
+                alt="Pony Mart"
+                className="h-2/3 w-full object-cover"
+              />
+            </a>
+            <p className="text-center pt-3">
+              E-commerce website, a group project created using the MERN stack
+            </p>
+          </div>
+          <div className="h-1/3">
+            <a
+              href="https://05-fear-js.vercel.app/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src={crud}
+                alt="CRUD"
+                className="h-2/3 w-full object-cover"
+              />
+            </a>
+            <p className="text-center pt-3">Simple CRUD manipulation</p>
+          </div>
+          <div className="h-1/3">
+            <a
+              href="https://5-fear-colmar.vercel.app/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src={colmar}
+                alt="Colmar"
+                className="h-2/3 w-full object-cover"
+              />
+            </a>
+            <p className="text-center pt-3">
+              My very first project with basic HTML, CSS
+            </p>
+          </div>
         </div>
       </div>
       <div className="hidden md:block">
@@ -55,7 +81,7 @@ const Project = ({ showProject }) => {
               <img src={colmar} alt="Colmar" className="h-2/3" />
             </a>
             <p className="text-center pt-3">
-              My very first front-end project in this journey
+              My very first project with basic HTML, CSS
             </p>
           </div>
         )}
