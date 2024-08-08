133adde5405b25005d4ec466f33fd1909bbf1f17
Bongkan <bongkan.jj@gmail.com>
2024-08-08 17:48:38 +0700

    edit experience desktop and align skills logo

--- src/components/experience/experienceDetails.jsx
+++ src/components/experience/experienceDetails.jsx
@@ -13,6 +13,7 @@ const ExperienceDetails = ({ showCompany }) => {
       {showCompany === "Generation" && (
         <div className="flex flex-col">
           <p className="pb-5 text-center">Apr 2024 – Jul 2024</p>
+          <p className="text-center pb-2">Junior Software Developer Bootcamp</p>
           <p>• Full Stack with MERN Stack</p>
           <p>• Software Development Cycle</p>
           <p>• Hands-on Learning from Industry experts</p>
@@ -21,6 +22,7 @@ const ExperienceDetails = ({ showCompany }) => {
       {showCompany === "KBTG" && (
         <div className="flex flex-col">
           <p className="pb-5 text-center">Sep 2023 – Dec 2023</p>
+          <p className="text-center pb-2">Infrastructure Engineer Bootcamp</p>
           <p>• DevOps Practices</p>
           <p>• Cloud Technology</p>
           <p>• Observability (Monitoring, Logging and Tracing)</p>
@@ -29,6 +31,9 @@ const ExperienceDetails = ({ showCompany }) => {
       {showCompany === "SVI" && (
         <div className="flex flex-col">
           <p className="pb-5 text-center">Oct 2021 – Aug 2022</p>
+          <p className="text-center pb-2">
+            Graduate Engineer – Program Management
+          </p>
           <p>• Project Improvment & Management</p>
           <p>• SMT-IMT Engineering</p>
           <p>• Cross Functional Team Management</p>
--- src/components/skills/skill.jsx
+++ src/components/skills/skill.jsx
@@ -10,45 +10,45 @@ import githublogo from "../../assets/Skills/github.svg";
 
 const Skill = () => {
   return (
-    <div className="w-full grid grid-cols-4 grid-rows-2 justify-center items-center animation-fade-in">
+    <div className="p-5 w-full grid-cols-3 grid-rows-3 grid md:grid-cols-4 md:grid-rows-2 justify-center items-center animation-fade-in">
       <img
         src={reactlogo}
-        className="w-2/3 hover:opacity-50 duration-100"
+        className="w-2/3 hover:opacity-50 duration-100 justify-self-center"
         alt=""
       />
       <img
         src={awslogo}
-        className="w-2/3 hover:opacity-50 duration-100"
+        className="w-2/3 hover:opacity-50 duration-100 justify-self-center"
         alt=""
       />
       <img
         src={csslogo}
-        className="w-2/3 hover:opacity-50 duration-100"
+        className="w-2/3 hover:opacity-50 duration-100 justify-self-center"
         alt=""
       />
       <img
         src={dockerlogo}
-        className="w-2/3 hover:opacity-50 duration-100"
+        className="w-2/3 hover:opacity-50 duration-100 justify-self-center"
         alt=""
       />
       <img
         src={htmllogo}
-        className="w-2/3 hover:opacity-50 duration-100"
+        className="w-2/3 hover:opacity-50 duration-100 justify-self-center"
         alt=""
       />
       <img
         src={javascriptlogo}
-        className="w-2/3 hover:opacity-50 duration-100"
+        className="w-2/3 hover:opacity-50 duration-100 justify-self-center"
         alt=""
       />
       <img
         src={postmanlogo}
-        className="w-2/3 hover:opacity-50 duration-100"
+        className="w-2/3 hover:opacity-50 duration-100 justify-self-center"
         alt=""
       />
       <img
         src={githublogo}
-        className="w-2/3 hover:opacity-50 duration-100"
+        className="w-2/3 hover:opacity-50 duration-100 justify-self-center"
         alt=""
       />
     </div>
