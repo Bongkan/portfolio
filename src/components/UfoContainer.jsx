0501e351bc400b76777658d1e58afa95529dd324
Bongkan <bongkan.jj@gmail.com>
2024-08-08 13:54:36 +0700

    edit ufo layout + header for each section

--- src/components/UfoContainer.jsx
+++ src/components/UfoContainer.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import Ufo from "../assets/ufo-flying-svgrepo-com.svg";
 
-const UfoContainer = () => {
+const UfoContainer = ({ activeSection }) => {
   return (
-    <div className="mt-3 p-0 flex justify-center">
+    <div className="mt-3 p-0 flex justify-center items-center">
       <img
         id="ufo-logo"
         src={Ufo}
         alt="ufo-logo"
         className="w-[50px] md:w-[75px]"
       />
+      <h1 className="pl-3 text-xl font-medium">{activeSection}</h1>
     </div>
   );
 };
